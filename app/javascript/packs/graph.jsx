9c319340b5fca970fed5752feb4913d0b9d35b7d
Tomek Maszkowski <tomek@wildbit.com>
2021-04-22 11:15:19 -0700

    Using latest react-d3-graph

--- app/javascript/packs/graph.jsx
+++ app/javascript/packs/graph.jsx
@@ -1,130 +1,122 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import PropTypes from "prop-types";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import PropTypes from 'prop-types'
 
-// See react-d3-graph docs at https://goodguydaniel.com/react-d3-graph/docs/index.html
-import { Graph as D3Graph } from "react-d3-graph";
+// See react-d3-graph docs at https://danielcaldas.github.io/react-d3-graph/docs/index.html
+import { Graph } from 'react-d3-graph'
 
 const D3_GRAPH_CONFIG = {
   linkHighlightBehavior: true,
   width: 900,
   height: 600,
   node: {
-    color: "yellow",
+    color: 'yellow',
     size: 120,
-    highlightStrokeColor: "blue"
+    highlightStrokeColor: 'blue',
   },
   link: {
-    highlightColor: "#efefef"
-  }
-};
+    highlightColor: '#efefef',
+  },
+}
 
+// custom prop types
+const NodeShape = PropTypes.shape({
+  id: PropTypes.string.isRequired,
+})
+
+const LinkShape = PropTypes.shape({
+  source: PropTypes.string.isRequired,
+  target: PropTypes.string.isRequired,
+})
+
+const SnaphotShape = PropTypes.shape({
+  nodes: PropTypes.arrayOf(NodeShape).isRequired,
+  links: PropTypes.arrayOf(LinkShape).isRequired,
+})
+
+// components
 const Inspector = ({ source, target, topics }) => (
   <p>
     {source && target ? (
-      `${source} and ${target} chatted about ${topics}`
+      <span>
+        <strong>{source}</strong> and <strong>{target}</strong> chatted about{' '}
+        <em>{topics}</em>
+      </span>
     ) : (
       <em>Hover your cursor over a connection line</em>
     )}
   </p>
-);
-
+)
 Inspector.propTypes = {
   source: PropTypes.string,
   target: PropTypes.string,
-  topics: PropTypes.string
-};
+  topics: PropTypes.string,
+}
 
-const NodeShape = PropTypes.shape({
-  id: PropTypes.string.isRequired
-});
+const App = ({ snapshot }) => {
+  const [currentSource, setCurrentSource] = React.useState()
+  const [currentTarget, setCurrentTarget] = React.useState()
 
-const LinkShape = PropTypes.shape({
-  source: PropTypes.string.isRequired,
-  target: PropTypes.string.isRequired
-});
+  const topics = 'Cheese and Wine' // FIXME
 
-const SnaphotShape = PropTypes.shape({
-  nodes: PropTypes.arrayOf(NodeShape).isRequired,
-  links: PropTypes.arrayOf(LinkShape).isRequired
-});
+  const handleClickNode = _nodeId => {}
 
-class Graph extends React.Component {
-  static propTypes = {
-    snapshot: SnaphotShape.isRequired
-  };
+  const handleMouseOverNode = _nodeId => {}
 
-  state = {
-    currentSource: undefined,
-    currentTarget: undefined
-  };
+  const handleMouseOutNode = _nodeId => {}
 
-  handleClickNode = nodeId => {};
+  const handleClickLink = (_source, _target) => {}
 
-  handleMouseOverNode = nodeId => {};
+  const handleMouseOverLink = (source, target) => {
+    setCurrentSource(source)
+    setCurrentTarget(target)
+  }
 
-  handleMouseOutNode = nodeId => {};
+  const handleMouseOutLink = (_source, _target) => {
+    setCurrentSource(undefined)
+    setCurrentTarget(undefined)
+  }
 
-  handleClickLink = (source, target) => {};
-
-  handleMouseOverLink = (source, target) => {
-    this.setState({
-      currentSource: source,
-      currentTarget: target
-    });
-  };
-
-  handleMouseOutLink = (source, target) => {
-    this.setState({
-      currentSource: undefined,
-      currentTarget: undefined
-    });
-  };
-
-  render() {
-    const { snapshot } = this.props;
-    const { currentSource, currentTarget } = this.state;
-
-    const topics = "Cheese and Wine"; // FIXME
-
-    return (
+  return (
+    <div>
       <div>
-        <div>
-          <Inspector
-            source={currentSource}
-            target={currentTarget}
-            topics={topics}
-          />
-        </div>
-        <D3Graph
-          id="graph"
-          data={snapshot}
-          config={D3_GRAPH_CONFIG}
-          onClickNode={this.handleClickNode}
-          onClickLink={this.handleClickLink}
-          onMouseOverNode={this.handleMouseOverNode}
-          onMouseOutNode={this.handleMouseOutNode}
-          onMouseOverLink={this.handleMouseOverLink}
-          onMouseOutLink={this.handleMouseOutLink}
+        <Inspector
+          source={currentSource}
+          target={currentTarget}
+          topics={topics}
         />
       </div>
-    );
-  }
+      <Graph
+        id='graph'
+        data={snapshot}
+        config={D3_GRAPH_CONFIG}
+        onClickNode={handleClickNode}
+        onClickLink={handleClickLink}
+        onMouseOverNode={handleMouseOverNode}
+        onMouseOutNode={handleMouseOutNode}
+        onMouseOverLink={handleMouseOverLink}
+        onMouseOutLink={handleMouseOutLink}
+      />
+    </div>
+  )
+}
+App.propTypes = {
+  snapshot: SnaphotShape.isRequired,
 }
 
 const DUMMY_SNAPSHOT = {
-  nodes: [{ id: "Harry" }, { id: "Sally" }, { id: "Alice" }],
+  nodes: [{ id: 'Harry' }, { id: 'Sally' }, { id: 'Alice' }],
   links: [
-    { source: "Harry", target: "Sally" },
-    { source: "Harry", target: "Alice" }
-  ]
-};
+    { source: 'Harry', target: 'Sally' },
+    { source: 'Harry', target: 'Alice' },
+  ],
+}
 
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
-    <Graph snapshot={SNAPSHOT_DATA || DUMMY_SNAPSHOT} />,
+    <App snapshot={SNAPSHOT_DATA || DUMMY_SNAPSHOT} />,
     document
-      .getElementById("layout-wrapper")
-      .appendChild(document.createElement("div"))
-  );
-});
+      .getElementById('layout-wrapper')
+      .appendChild(document.createElement('div'))
+  )
+})
